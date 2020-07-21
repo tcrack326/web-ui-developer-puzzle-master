1d737274f06ef9efcaf15d9313851f62e27a121b
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2020-07-21 10:32:34 -0400

    ran unit tests

---
+++ libs/books/data-access/src/lib/books-data-access/books-data-access.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromBooks from './books/books.reducer';
+import { BooksEffects } from './books/books.effects';
+import * as fromReadingList from './reading-list/reading-list.reducer';
+import { ReadingListEffects } from './reading-list/reading-list.effects';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromBooks.BOOKS_FEATURE_KEY, fromBooks.reducer),
+    StoreModule.forFeature(
+      fromReadingList.READING_LIST_FEATURE_KEY,
+      fromReadingList.reducer
+    ),
+    EffectsModule.forFeature([BooksEffects, ReadingListEffects])
+  ]
+})
+export class BooksDataAccessModule {}
