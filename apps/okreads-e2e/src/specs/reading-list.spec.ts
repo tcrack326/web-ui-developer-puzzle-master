3d0a09854a5433878ddab385d24d850aa4ff8fd4
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2020-07-22 11:25:36 -0400

    added e2e test for feature

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,4 +1,4 @@
-import { $, browser, ExpectedConditions } from 'protractor';
+import { $, $$, browser, ExpectedConditions } from 'protractor';
 
 describe('When: I use the reading list feature', () => {
   it('Then: I should see my reading list', async () => {
@@ -26,7 +26,29 @@ describe('When: I mark a book as finished feature', () => {
       ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
     );
 
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+
     const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    const readingListItems = await $$('.reading-list-item');
+    const searchItems = await $$('[data-testing="book-item"] button');
+
+    searchItems[0].click();
     await readingListToggle.click();
+
+    const finishButton = await $('[data-testing="finish-button"]');
+    await finishButton.click();
+
+    const finishText = await $('[data-testing="finish-date"]');
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(finishText, 'Finished: ' + new Intl.DateTimeFormat('en-US').format(new Date()))
+    );
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('[data-testing="add-button"]'), 'Finished')
+    );
   });
 });
